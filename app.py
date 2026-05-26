4a4596078e50a1e9197e3c5a5b3dfb599c01addb
agusmalpassi2-byte <agusmalpassi2@gmail.com>
2026-05-26 19:43:32 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,23 @@
 import streamlit as st
+import rasterio
+import numpy as np
+import matplotlib.pyplot as plt
 
 st.title("🌾 Ambientación automática")
 
-st.write("La app funciona correctamente")
+archivo = st.file_uploader(
+    "Subí un raster VARI/NDVI",
+    type=["tif"]
+)
+
+if archivo:
+
+    with rasterio.open(archivo) as src:
+        raster = src.read(1)
+
+    st.write("Raster cargado correctamente")
+
+    fig, ax = plt.subplots()
+    ax.imshow(raster)
+
+    st.pyplot(fig)
